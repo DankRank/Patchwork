d43f9e25709f23ec2e8062408c6e778df9b81654
Egor <egor@opensrc.club>
2020-09-29 17:39:59 +0300

    Add cli utility for patching

---
+++ pw/Program.cs
@@ -0,0 +1,28 @@
+﻿using Patchwork;
+using Patchwork.Utility;
+using Serilog;
+
+namespace pw
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            if (args.Length < 3) {
+                Log.Logger.Information("Usage: pw INPUT OUTPUT PATCH [PATCH...]");
+                return 1;
+            }
+            var progress = new ProgressObject();
+            progress.TaskTitle.HasChanged += (obj) => Log.Logger.Information("-- " + obj.Value);
+            progress.TaskText.HasChanged += (obj) => Log.Logger.Information(obj.Value);
+            string target = args[0];
+            string output = args[1];
+            var patcher = new AssemblyPatcher(target);
+            for (int i = 2; i < args.Length; i++)
+                patcher.PatchAssembly(args[i], progress);
+            patcher.WriteTo(output);
+            return 0;
+        }
+    }
+}
